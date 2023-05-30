281aee0cfddddc83cb976e5145a7291961beb8a7
Gabriela Tomaz do Amaral Ribeiro <32981034+gabrielatomaz@users.noreply.github.com>
2023-05-30 14:20:47 +0000

    adding booklet script changes

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 $(function() {
 
   $('#mybook')
-  .booklet({ 
-    height: 800, 
-    width: 1200, 
-    autoCenter: true, 
-    closed: true,
-    pageBorder: -9
-  });
+    .booklet({ 
+      height: 800, 
+      width: 1200, 
+      autoCenter: true, 
+      closed: true,
+      pageBorder: -9,
+      startingPage: 2,
+      manual: true
+    });
 
 });
   
